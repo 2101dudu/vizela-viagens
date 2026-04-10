1b385ebc1de2b75199831d5f0c428e633df40b6e
Eduardo Faria <eduardo21faria@gmail.com>
2026-04-10 15:44:39 +0000

    feat(tarmac): public prod release

--- app/config.ts
+++ app/config.ts
@@ -2,4 +2,4 @@ const isServer = typeof window === 'undefined';
 
 export const API_BASE_URL = isServer
   ? "http://tarmac:8080/api"
-  : (process.env.NEXT_PUBLIC_API_BASE_URL || "https://tarmac.blackmesa.local/api");
+  : (process.env.NEXT_PUBLIC_API_BASE_URL || "https://api.eduardofaria.com/api");
